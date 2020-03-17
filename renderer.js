67a71fa5cba0f4de502aec8a95759adcc5b8cd0d
Xuntron <seeratsdsking@gmail.com>
2020-03-17 17:02:08 +0500

    send url from renderer to main

--- renderer.js
+++ renderer.js
@@ -1,6 +1,17 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+const { ipcRenderer } = require('electron')
+// console.log(ipcRenderer.sendSync('synchronous-message', 'ping')) // prints "pong"
+
+// ipcRenderer.on('asynchronous-reply', (event, arg) => {
+//     console.log(arg) // prints "pong"
+// })
+// ipcRenderer.send('asynchronous-message', 'ping')
+
+
+document.getElementById("urlform").addEventListener("submit", (e) => {
+    e.preventDefault();
+    let url = document.getElementById("urlfield").value;
+
+    ipcRenderer.send('url', url)
+});
+
+
